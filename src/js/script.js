7ba92537352c99be823ef2b2bca62c93e9109ad8
Tomek Goatowski <tomaszkozlowski.tech@gmail.com>
2022-11-29 21:19:42 +0100

    Accordion

--- src/js/script.js
+++ src/js/script.js
@@ -89,23 +89,14 @@
     initAccordion() {
       const thisProduct = this;
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
-    /* START: add event listener to clickable trigger on event click */
-    clickableTrigger.addEventListener('click', function(event) {
-      /* prevent default action for event */
-      event.preventDefault();
-
-      /* find active product (product that has active class) */
-      const activeProduct = document.querySelector(select.menuProduct.activeProduct)
-
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-      if (activeProduct && activeProduct!==thisProduct.element) {
-        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-      }
-
-      /* toggle active class on thisProduct.element */
-      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive)
-    });
+      clickableTrigger.addEventListener('click', function(event) {
+        event.preventDefault();
+        const activeProduct = document.querySelector(select.menuProduct.activeProduct);
+        if (activeProduct && activeProduct!==thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
 
   }
